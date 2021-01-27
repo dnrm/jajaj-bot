6cff6d22444ddd6fad0855b818cd0588a7e14bed
Daniel Medina <daniel@medina.com>
2021-01-27 11:38:57 -0600

    Added configuration lock mode

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const Discord = require('discord.js')
 const Client = new Discord.Client();
 
 let onOrOff = false;
+let adminMode = false;
 
 Client.on('message', (message) => {
-    if (message.author.username != Client.user.username) {
-        if (message.content == '--on') {
-            onOrOff = true;
-            message.channel.send('Turned on :thumbsup:');
-        } else if (message.content == '--off') {
-            onOrOff = false;
-            message.channel.send('Turned off :thumbsup:');
+    if (adminMode == false) {
+        if (message.author.username != Client.user.username) {
+            if (message.content == '--on') {
+                onOrOff = true;
+                message.channel.send('Turned on :thumbsup:');
+            } else if (message.content == '--off') {
+                onOrOff = false;
+                message.channel.send('Turned off :thumbsup:');
+            }
+        }
+    }
+    if (message.author.username == 'dnrm') {
+        if (message.content == '--locked') {
+            adminMode = true;
+            message.channel.send('Locked configuration :thumbsup:');
+        } else if (message.content == '--unlocked') {
+            adminMode = false;
+            message.channel.send('Unlocked configuration :thumbsup:');
         }
     }
 })
